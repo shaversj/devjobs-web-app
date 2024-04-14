1ae87977d0f60d82adc28fc94676ce42f9ba1f05
Josh Shavers <shaversj@gmail.com>
2024-04-14 11:19:38 -0500

    Initial Commit + Tailwind Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        h1: "28px",
+        h2: "24px",
+        h3: "20px",
+        h4: "14px",
+      },
+      leading: {
+        body: "26px",
+        h1: "34px",
+        h2: "29px",
+        h3: "24px",
+        h4: "24px",
+        h5: "18px",
+      },
+      colors: {
+        "primary-violet": "#4851b6",
+        "primary-light-violet": "#939bf4",
+        "primary-very-dark-blue": "#19202D",
+        "primary-midnight": "#121721",
+        "secondary-light-grey": "#f4f6f8",
+        "secondary-grey": "#9daec2",
+        "secondary-dark-grey": "#6e8098",
+      },
+      fontFamily: {
+        kumbhSans: ["var(--font-kumbhSans)"],
       },
     },
   },
