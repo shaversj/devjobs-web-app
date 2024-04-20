5c8b00408e8b338755576dc548e4c95c1bfc1e15
Josh Shavers <shaversj@gmail.com>
2024-04-20 10:55:05 -0500

    Add query to filter by title

--- src/app/_components/actions.js
+++ src/app/_components/actions.js
@@ -1,4 +1,12 @@
+import { db } from "@/app/db";
+import { jobsTable } from "@/app/db/drizzle.schema";
+import { like, sql } from "drizzle-orm";
+
 export async function getJobsByTitle(title, data) {
-  if (!title) return data;
-  return data.filter((job) => job.position.toLowerCase().includes(title.toLowerCase()));
+  if (!title) return await db.select().from(jobsTable).execute();
+  return await db
+    .select()
+    .from(jobsTable)
+    .where(like(sql`lower(${jobsTable.position})`, `%${title.toLowerCase()}%`))
+    .execute();
 }
