b2def9361e41bcfeb18cc4cbf8ba0a042cd13941
Josh Shavers <shaversj@gmail.com>
2024-04-20 09:26:01 -0500

    Add Search Filter

---
+++ src/app/_components/SearchFilter.js
@@ -0,0 +1,40 @@
+"use client";
+
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+
+export default function SearchFilter() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = (term) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("jobFilter", term);
+    } else {
+      params.delete("jobFilter");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <div className={"flex h-[80px] -translate-y-1/2 items-center rounded-md bg-white pl-6 pr-4"}>
+      <input
+        className={"h-[20px] w-[105px] text-secondary-dark-grey focus:outline-none"}
+        placeholder={"Filter by title"}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("jobFilter")?.toString()}
+      />
+      <div className={"ml-auto flex gap-x-6"}>
+        <button>
+          <img className={"h-[20px] w-[20px]"} src={"/assets/search/icon-filter.svg"} alt={"filter"} />
+        </button>
+        <button>
+          <img className={"h-[48px] w-[48px]"} src={"/assets/search/icon-search.svg"} alt={"search"} />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import mydata from "../assets/data.json";
 import CardList from "@/app/_components/CardList";
 import Header from "@/app/_components/Header";
 import Job from "@/app/_components/Job";
+import SearchFilter from "@/app/_components/SearchFilter";
 
 export default function Home() {
   return (
     <>
       <div className={"min-h-screen bg-secondary-light-grey px-6"}>
-        <Job />
+        <SearchFilter />
+        {/*<Job />*/}
         {/*<CardList data={mydata} />*/}
       </div>
     </>
