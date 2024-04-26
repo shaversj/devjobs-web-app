fc5e48e712f4b32bc0f5b3172d28eb148bf2d0bb
Josh Shavers <shaversj@gmail.com>
2024-04-26 08:09:14 -0500

    add toggle icons

--- src/app/components/Toggle.js
+++ src/app/components/Toggle.js
@@ -5,13 +5,21 @@ export default function Toggle() {
   const [enabled, setEnabled] = useState(false);
 
   return (
-    <Switch
-      checked={enabled}
-      onChange={setEnabled}
-      className={`${enabled ? "bg-blue-600" : "bg-gray-200"} relative inline-flex h-6 w-11 items-center rounded-full`}
-    >
-      <span className="sr-only">Enable notifications</span>
-      <span className={`${enabled ? "translate-x-6" : "translate-x-1"} inline-block h-4 w-4 transform rounded-full bg-white transition`} />
-    </Switch>
+    <>
+      <div className={"flex items-center gap-x-4"}>
+        <img src={"/assets/desktop/icon-sun.svg"} alt={"sun"} />
+        <Switch
+          checked={enabled}
+          onChange={setEnabled}
+          className={`${enabled ? "bg-white" : "bg-white"} relative inline-flex h-6 w-11 items-center rounded-full`}
+        >
+          <span className="sr-only">Enable notifications</span>
+          <span
+            className={`${enabled ? "translate-x-6" : "translate-x-1"} inline-block h-3.5 w-3.5 transform rounded-full bg-[#5963df] transition`}
+          />
+        </Switch>
+        <img className={"h-3 w-3"} src={"/assets/desktop/icon-moon.svg"} alt={"moon"} />
+      </div>
+    </>
   );
 }
