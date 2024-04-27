cc7fafcfa66c5e242dd84d75998815f08e00d948
Josh Shavers <shaversj@gmail.com>
2024-04-27 18:11:03 -0500

    Add optional request context

--- src/app/db/client.js
+++ src/app/db/client.js
@@ -1,4 +1,5 @@
-import { getRequestContext } from "@cloudflare/next-on-pages";
+import { getOptionalRequestContext, getRequestContext } from "@cloudflare/next-on-pages";
 import { drizzle } from "drizzle-orm/d1";
+const { env } = getOptionalRequestContext() ?? { env: {} };
 
-export const db = drizzle(getRequestContext().env.DB);
+export const db = drizzle(env.DB);
