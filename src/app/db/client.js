36feb1afadee93a427711025990574f6e0a10f9e
Josh Shavers <shaversj@gmail.com>
2024-04-28 09:15:18 -0500

    Update scripts

--- src/app/db/client.js
+++ src/app/db/client.js
@@ -1,5 +1,5 @@
 import { getOptionalRequestContext } from "@cloudflare/next-on-pages";
 import { drizzle } from "drizzle-orm/d1";
-const { env } = getOptionalRequestContext() ?? { env: {} };
 
+const { env } = getOptionalRequestContext() ?? { env: {} };
 export const db = drizzle(env.DB);
